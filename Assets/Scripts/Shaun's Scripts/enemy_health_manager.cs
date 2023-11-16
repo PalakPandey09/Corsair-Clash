2d4e9588e7a5721520b40277a8883fc6db8ad00a
ironwerkz <80446613+ironwerkz@users.noreply.github.com>
2023-11-15 16:22:13 -0800

    unstable don't pull

--- Assets/Scripts/Shaun's Scripts/enemy_health_manager.cs
+++ Assets/Scripts/Shaun's Scripts/enemy_health_manager.cs
@@ -4,23 +4,22 @@ using log4net.Core;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class enemy_health_manager : MonoBehaviour
-{
-    //class that creates enemies with their level appropriate health bars
-    public class enemy_health_bar
-    {   
+{     
+        //game object the health bar sits on
+        public GameObject enemy;
+        
+        
         //audio source
         private AudioSource audioSource;
-        //establishes the canvas that the image will fill
-        private GameObject canvas;
-        // Load the image from prefab folder
-        Image health_bar = Resources.Load("Assets/Resources/health_bar.prefab") as Image;
-        
-        
+        public AudioClip death;
+        //health bar
+        public Image health_bar;
         //enemy health to be assigned
         private float enemy_health;
         //determines what unit level health is assigned
-        public int unit_levels;
+        private int unit_levels;
         //keeps track current unit level;
         private float current_level;
         //unit health per current unit health
@@ -31,11 +30,14 @@ public class enemy_health_manager : MonoBehaviour
         private float unit_5 = 30f;
         
         public void unit_health()
-        { 
-            //switch statement checks for current player health
-            //and then assigns its health in accordance to each level
-            switch (unit_levels)
-            {
+        {  
+           //gets unit level from object the script is assigned to 
+           EnemyUnitTargeting playerUnitTargeting= enemy.GetComponent<EnemyUnitTargeting>();
+           unit_levels = playerUnitTargeting.levelOfUnit;
+           //switch statement checks for current player health
+           //and then assigns its health in accordance to each level
+           switch (unit_levels)
+           {
                 case ( > 0) when unit_levels == 1:
                     enemy_health = unit_1;
                     current_level = unit_1;
@@ -56,41 +58,51 @@ public class enemy_health_manager : MonoBehaviour
                     enemy_health = unit_5;
                     current_level = unit_5;
                     break;
-            }
-            //creates a canvas, then loads the health_bar to it.
-            GameObject newCanvas = Instantiate(canvas) as GameObject;
-            Image canvas_health_bar = Instantiate(health_bar);
-            canvas_health_bar.transform.SetParent(newCanvas.transform, true);
-            
-            //removes health when unit dies
-            if(enemy_health <= 0)
-            {
-                audioSource.Play();
-                canvas.SetActive(false);
-            }
+           }
             
         }
-         
-        public void OnCollisionEnter2D(Collision2D collision)
+    
+
+    public void OnCollisionEnter2D(Collision2D collision)
         {
-            //takes damage value of collision projectile
-            enemy_health -= collision.gameObject.GetComponent<BulletManager>().bulletDamage;
-            //updates current health
-            health_bar.fillAmount =  enemy_health / current_level;
-        }   
-        
-    }
+        //takes damage value of collision projectile
+        enemy_health -= collision.gameObject.GetComponent<BulletManager>().bulletDamage;
+        //updates current health
+        health_bar.fillAmount =  enemy_health / current_level;
+        } 
     
     // Start is called before the first frame update
     void Start()
-    {
-        
+    {   
+        unit_health();
+        //finds position of unit the health bar is attached to
+        Vector3 localPosition = enemy.transform.localPosition;
+        //pulls image prefab from resource folder
+        Image createImage = Instantiate(health_bar);
+        createImage.transform.SetParent(GameObject.Find("Canvas").transform);
+        //GameObject.Find("health_bar").SetActive(true);
+        //sets health bar position slightly above its unit
+        localPosition.y += 1;
+        createImage.transform.localPosition = localPosition;
+        createImage.transform.localScale = new Vector3(0.4f, 0.4f, 0.4f);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+
+        //removes health when unit dies
+        if(enemy_health <= 0)
+        {
+            audioSource.clip = death;
+            audioSource.Play(); 
+            //GameObject.Find("health_bar").SetActive(false);
+            BattleManager.instance.numPlayersDestroyed++;
+            Destroy(this.gameObject);
+        }
+            
     }
 
     
